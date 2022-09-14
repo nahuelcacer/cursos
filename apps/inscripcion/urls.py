24c684c0ba98ba8f9d34466fb9fbfd99d877d3b5
Nahuel <nahue_cge@hotmail.com>
2022-09-14 15:42:00 -0300

    intento de ingegracion de mp

--- apps/inscripcion/urls.py
+++ apps/inscripcion/urls.py
@@ -6,5 +6,7 @@ app_name = 'apps.inscripcion'
 
 urlpatterns = [
     path('<int:id>', views.Inscribirse.as_view(), name="inscribirse"),
-    path('listar/<int:id>', views.Listar.as_view(), name="listar")
+    path('listar/<int:id>', views.Listar.as_view(), name="listar"),
+    #pagar
+    path(r'<int:id>/pay', views.Pay.as_view(),name="pay"),
 ]
--- apps/inscripcion/views.py
+++ apps/inscripcion/views.py
@@ -1,4 +1,5 @@
 from multiprocessing import context
+from urllib.parse import urlencode
 from django.shortcuts import redirect, render
 from django.views import View
 from apps.administracion.models import Cursos
@@ -6,34 +7,14 @@ from apps.inscripcion.models import Inscripciones
 from django.urls import reverse
 from django.http import HttpResponse
 from apps.inscripcion.forms import Inscripcion
-
+import urllib
+ 
 # SDK de Mercado Pago
-# import mercadopago
+import mercadopago
 # # Agrega credenciales
-# sdk = mercadopago.SDK("TEST-3189877754485702-090914-7182f92c2075e9fce5b9ba0f34445232-1195365420")
+sdk = mercadopago.SDK("TEST-3189877754485702-090914-7182f92c2075e9fce5b9ba0f34445232-1195365420")
 # Crea un ítem en la preferencia
-# preference_data = {
-        #     "items": [
-        #         {
-        #             "title": cursos.nombre,
-        #             "quantity": 1,
-        #             "unit_price": 1
-        #             #  "unit_price": int(cursos.precio)
-        #         }
-        #     ],
-        #     "back_urls": 
-        #                         {
-        #                             "success": "http://127.0.0.1:8000/inscripcion/",
-        #                             "failure": "http://www.failure.com"                              
-        #                         },
-        
-        #     "auto_return": "approved",
-        #     "binary_mode": True
-        # }
 
-        # preference_response = sdk.preference().create(preference_data)
-        # preference = preference_response["response"]
-        # print(preference)
 
 class Inscribirse(View):
     def get(self,request,id):
@@ -55,8 +36,9 @@ class Inscribirse(View):
             if form.is_valid():
                 aux = form.save(commit=False)
                 aux.curso = curso
-                aux.save()
-        return redirect('apps.administracion:addCurso')
+                # aux.save()
+
+                return redirect('apps.inscripcion:pay', id)
 
 class Listar(View):
     def get(self,request,id):
@@ -73,3 +55,31 @@ class Listar(View):
             return render(request,'inscripcion/listar.html', context)
         else:
             return redirect('/')
+
+class Pay(View):
+    def get(self,request,id):
+        curso = Cursos.objects.get(id=id)
+       
+        preference_data = {
+            "items": [
+                {
+                    "title": curso.nombre,
+                    "quantity": 1,
+                    "unit_price": 1
+                    #  "unit_price": int(cursos.precio)
+                }
+            ],
+            "back_urls": 
+                                {
+                                    "success": "http://127.0.0.1:8000/inscripcion/",
+                                    "failure": "http://www.failure.com"                              
+                                },
+        
+            "auto_return": "approved",
+            "binary_mode": True
+        }
+
+        preference_response = sdk.preference().create(preference_data)
+        preference = preference_response["response"]
+        # print(preference)
+        return render(request,'inscripcion/pay.html')
